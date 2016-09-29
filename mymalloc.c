0bfc1e0a3b5ddb719f2a59c2d35a795c0262f94a
Jason Tuhy <jasontuhy@gmail.com>
2016-09-29 19:26:57 -0400

    I WROTE MALLOC TOM I DID IT

--- mymalloc.c
+++ mymalloc.c
@@ -3,8 +3,54 @@
 #include "mymalloc.h"
 #define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
 #define free( x ) myfree( x, __FILE__, __LINE__ )
-static char myblock[5000];
+#define SIZE 5000
 
-void * mymalloc(int bytes){
-	
+static char myblock[SIZE];
+
+
+void * split(int * p, int bytesOrOne){
+	*p = bytesOrOne;
+	void * toReturn = (void *) p+4;
+	int * p1 = (int *) (&myblock[(4 + bytesOrOne)]);
+	int newValue = (SIZE - (8 + bytesOrOne));
+	if (newValue % 2 == 1){
+		newValue++;
+	}
+	*p1 = newValue;
+	return toReturn;
+}
+
+void * mymalloc(int bytes, char * file, int line){
+	if (bytes % 2 == 1){bytes++;}
+	int * p = (int *) (&myblock[0]);
+	int bytesOrOne = (bytes | 1);
+	if (*p == 0){
+		return split(p,bytesOrOne);
+	}
+	while((void *)p < (void *)&myblock[SIZE]){
+		if ((*p & 1) == 1 || *p < bytes){
+			p = (int *) (&myblock[(*p + 4)]);
+		}
+		else if(*p > bytes){
+			return split(p,bytesOrOne);
+		}
+	}
+	return NULL;
+}
+
+
+int main(){
+	char * a = malloc(8);
+	// int * intlocation = (int*)(&myblock[13]);
+	// printf("%d\n", *intlocation);	
+	char * b = malloc(15);
+	// int * intlocation = (int*)(&myblock[13]);
+	// printf("%d\n", *intlocation);	
+	char * c = malloc(2000);
+	int * p = (int*)(&myblock[0]);
+	while((void *)p < (void *)&myblock[SIZE] && *p != 0){
+		printf("%d\n", *p);	
+		p = (int *) (&myblock[(*p + 4)]);
+	}
+	return 0;
 }
