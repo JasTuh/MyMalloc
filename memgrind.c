46602ffea361839afcca8378c96acba5576a7235
Jason Tuhy <jasontuhy@gmail.com>
2016-10-11 20:11:25 -0400

    TOM I MADE IT RECURSIVE

--- memgrind.c
+++ memgrind.c
@@ -1,48 +1,153 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "mymalloc.h"
 #include "mymalloc.c"
 #define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
 #define free( x ) myfree( x, __FILE__, __LINE__ )
-#define SIZE 5000
+#define SIZE 25000
 
 
 int testA(){
-	char * a[20];
-	int i =0;
-	for (;i<20;i++){
-		a[i] = (char *) malloc(1);
-		if (a[i] == NULL){
-			return -1;
-		}
-		printf("a[%d]=%d\n", i,(a[i] - ((int) (a[0]-4)) ));
-		int * p = (int *) (a[i] - 4);
-		printf("implicit int = %d\n", *p); //this should not be 0 :(
+	int total = 0, counter = 0;
+	while (counter < 100){
+		clock_t start = clock(), diff;
+		char * a[3000];
+		int i =0;
+		for (;i<3000;i++){
+			a[i] = (char *) malloc(1);
+			if (a[i] == NULL){
+				return -1;
+			}
+			// printf("a[%d]=%d\n", i,(a[i] - ((int) (a[0]-4)) ));
+			// int * p = (int *) (a[i] - 4);
+			// printf("a[%d]=%d\n", i, a[i]);
+			// printf("p=%d\n", p);
+			// printf("implicit int = %d\n", *p); //this should not be 0 :(
 
+		}
+		i =0;
+		// int * value = (int *) (a[0]-4);
+		for (;i<3000;i++){
+			free(a[i]);
+			// printf("a[%d]=%d\njust tried to free %d\n", 0, *value, i);
+		}
+		diff = clock() - start;
+		total+=diff;
+		counter++;
 	}
-	i =0;
-	int * value = (int *) (a[0]-4);
-	for (;i<20;i++){
-		free(a[i]);
-		printf("a[%d]=%d\njust tried to free %d\n", 0, *value, i);
-	}
-	return 0;
+	return total/100;
 }
 int testB(){
-	int i = 0;
-	for(;i < 100; i++){
-		char * a = (char *) malloc(sizeof(char));
-		free(a);
+	int total = 0, counter = 0;
+	while (counter < 100){
+		clock_t start = clock(), diff;
+		int i = 0;
+		for(;i < 3000; i++){
+			char * a = (char *) malloc(sizeof(char));
+			free(a);
+		}
+		diff = clock() - start;
+		total += diff;
+		counter++;
 	}
-	return 0;
+	return total/100;
+	
 }
 int testC(){
+	int total = 0, counter = 0;
+	char * a[3000];
+	srand(time(NULL));
+	while (counter < 100){
+		clock_t start = clock(), diff;
 
+		int r = rand() % 10, i = 0, freeIndex = 0, mallocIndex = 0; 
+
+		while (i < 3000){
+			if (mallocIndex == 3000){
+				if (freeIndex < 3000){
+					printf("before1\n");
+					free(a[freeIndex++]);
+					printf("after1\n");
+				}
+			}
+			else if (freeIndex >= mallocIndex){
+				if (mallocIndex < 3000){
+					printf("before2\n");
+					a[mallocIndex++] = (char *) malloc(1);
+					if (a[mallocIndex-1] == NULL){
+						printf("\n\nITS NULL\n\n\n");
+					}
+					printf("after2\n");
+				}
+			} else if (r > 4){
+				if (mallocIndex < 3000){
+					printf("before3\n");
+					a[mallocIndex++] = (char *) malloc(1);
+					if (a[mallocIndex-1] == NULL){
+						printHeap();
+						printf("\n\nITS NULL\n\n\n");
+					}
+					printf("after3\n");
+				}
+			}
+			else{
+				if (freeIndex < 3000){
+					int * value = (int *) (a[freeIndex]-4);
+					printf("before4 freeIndex = %d mallocIndex=%d and value = %d\n", freeIndex, mallocIndex, *value);
+					free(a[freeIndex++]);
+					value = (int *) (a[freeIndex-1]-4);
+					printf("after4 value = %d\n", *value);
+				}
+			}
+			r = rand() % 10;
+			i++;
+		}
+		printf("counter = %d\n", counter);
+		diff = clock() - start;
+		// return diff;
+		total += diff;
+		counter++;
+	}
+	return total/100;
 }
 int main(){
-	printf("%d\n", testA());
-	printf("ASFASDKLFJADSF\n");
-	printf("%d\n", testB());
+	// printf("%d\n", testA());
+	// printf("%d\n", testB());
+	// printf("%d\n", testC());
+	char * a[10];
+	printf("1\n");
+	printHeap();
+	a[0] = malloc(1);
+	printf("2\n");
+	printHeap();
+	a[1] = malloc(1);
+	printf("3\n");
+	printHeap();
+	a[2] = malloc(1);
+	printf("4\n");
+	printHeap();
+	a[3] = malloc(1);
+	free(a[0]);
+	free(a[2]);
+	printf("5\n");
+	printHeap();
+	printf("hopefully freezes\n");
+	free(a[1]);
+	printf("after freezes\n");
+	printHeap();
+	// free(a[0]);
+	// printf("5\n");
+	// printHeap();
+	// free(a[1]);
+	// printf("6\n");
+	// printHeap();
+	// a[4] = malloc(1);
+	// printf("7\n");
+	// printHeap();
+	// a[5] = malloc(1);
+	// printf("8\n");
+	// printHeap();
 	// char * a = malloc(sizeof(char));
 	// printf("a=%d\n", (a - 6295648));
 	// char * b = malloc(15);
--- mymalloc.c
+++ mymalloc.c
@@ -3,14 +3,21 @@
 #include "mymalloc.h"
 #define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
 #define free( x ) myfree( x, __FILE__, __LINE__ )
-#define SIZE 5000
+#define SIZE 25000
 
 static char myblock[SIZE];
 
 void * split(int * p, int bytesOrOne, int toAdd){
+	if (*p == (bytesOrOne-1)){
+		*p = bytesOrOne;
+		void * toReturn = ((void *)p)+4;
+		return toReturn;
+	}
 	int oldValue = *p == 0 ? (SIZE-4) : *p;
 	*p = bytesOrOne;
-	void * toReturn = (p+4);
+	// printf("setting memory address %d to %d\n", p, bytesOrOne);
+
+	void * toReturn = ((void *)p)+4;
 	int * p1 = (int *) (&myblock[(toAdd + 4 + bytesOrOne - 1)]);
 	int newValue = (oldValue - (4 + (bytesOrOne - 1)));
 	if (newValue < 0){
@@ -18,6 +25,7 @@ void * split(int * p, int bytesOrOne, int toAdd){
 		return toReturn;	
 	}
 	else{ 
+		// printf("setting memory address %d to %d\n", p1, newValue);
 		*p1 = newValue;
 	}
 	return toReturn;
@@ -45,19 +53,23 @@ void * mymalloc(int bytes, char * file, int line){
 			return split(p,bytesOrOne, toAdd);
 		}
 	}
-	printf("%s made this function error on line %d\n", file, line);
+	// printf("%s made this function error on line %d\n", file, line);
 	return NULL;
 }
 void combine(){
 	int * current = (int*)(&myblock[0]);
 	int * prev = NULL;
 	int toAdd = 0;
+	printHeap();
+	int callAgain = 0;
 	while((void *)current < (void *)&myblock[SIZE] && *current != 0){	
 		int currentValue = *current;
 		int prevValue = prev == NULL ? 1:*prev;
 		if (currentValue % 2 == 0 && (prevValue % 2 == 0)){
 			*prev = currentValue+prevValue + 4;
 			current = (void *)(&myblock[(toAdd + currentValue + 4)]);
+			callAgain = 1;
+			break;
 		}
 		else{
 			prev = current;
@@ -66,18 +78,21 @@ void combine(){
 			}
 			current = (int *) (&myblock[(toAdd + (currentValue) + 4)]);
 			toAdd += currentValue + 4;
-		}
 
+		}
+	}
+	if (callAgain){
+		combine();
 	}
 }
 void printHeap(){
 	int * p = (int*)(&myblock[0]);
-	int i = 0;
-	for (; i < 100; i++){
-		p = (int *) (&myblock[i]);
-		if (*p != 0){
-			printf("found a %d at %d\n", *p, i);
-		}
+	int toAdd = 0;
+	while ((void *)p < (void *)&myblock[SIZE-1] && *p != 0){		
+		int oldValue = *p;
+		printf("found a %d at %d\n", *p, ((void *)p - (void *)&myblock[0]));
+		p = (int *) (&myblock[(toAdd+(oldValue&~1) + 4)]);
+		toAdd+=(oldValue&~1)+4;
 	}
 }
 void myfree(void * toFree, char * file, int line){
